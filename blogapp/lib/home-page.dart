03f37d6c6757e15cf38b39a23058136724633a3f
nikkon-roy <nikkonlawrance@gmail.com>
2022-09-07 00:19:34 +0600

    Second Commite

--- blogapp/lib/home-page.dart
+++ blogapp/lib/home-page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,8 +11,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+    static const Map<String, String> defaultHeader = {
+    "Accept": "application/json",
+  };
+   Map<String ,dynamic> ?myMap;
+
+     fetchBlogData()async{
+     var url='https://apitest.hotelsetting.com/api/admin/blog-news';
+     var responce1=await http.get(Uri.parse(url),headers: {
+      "Accept": "application/json",
+      //"Authorization": "bearer 2|2V1N1HjXJZcTvYZkmCh3zgM867XPDqMp2HXShMbj",
+    }); 
+     print("all data are ${responce1.body}");
+    
+    
+  
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    fetchBlogData();
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        centerTitle: true,
+      ),
+    );
   }
 }
